a2f074cbc66948334c61532a599b518403b206d4
Jerry McKee Jr <JerryMcKeeJr.Dev@gmail.com>
2023-10-16 17:38:22 -0400

    built out HTML and getValues function

--- js/site.js
+++ js/site.js
@@ -1,12 +1,29 @@
-function displayMessage() {
+function getValues() {
     // get the text out of input and assign to a variable
-    let msg = document.getElementById('message').value;
-    
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'APP NAME',
-            text: msg, // <-- replace this with the variable
-        }
-    );
+    let msg = document.getElementById('userInput').value;
+
+    // check to make sure input is not empty
+    if(msg.length == 0) {
+        Swal.fire(
+            {
+                icon: 'error',
+                backdrop: false,
+                title: 'Oops',
+                text: 'Please enter in a message'
+            });
+    }
+    else {
+        // send the input to be checked if it is a palindrome
+        let results = checkForPalindrome(msg);
+        // send results to be displayed
+        displayResults(results);
+    }   
+}
+
+function checkForPalindrome() {
+
+}
+
+function displayResults() {
+
 }
