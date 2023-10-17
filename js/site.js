e0578b716ac7259a2c6b3a01f0a87413978615da
Jerry McKee Jr <JerryMcKeeJr.Dev@gmail.com>
2023-10-17 00:36:00 -0400

    base program completed

--- js/site.js
+++ js/site.js
@@ -21,34 +21,36 @@ function getValues() {
 }
 
 function checkForPalindrome(msg) {
-    
+    // variable created for output
     let output = '';
     
     
-    // for loop
+    // for loop to run through all characters of a string
     for(let i = msg.length - 1; i >= 0; i--){
         let letter = msg[i];
         output += letter
     }
     
-    // check to see if msg and output the same forwards and backwards
-    if(output.toLowerCase() != msg.toLowerCase()){
-        return [false, '']
+    // check to see if msg and output the same forwards and backwards regardless of spaces or Capital letters or special characters
+    // return results of the check AND the reversed string 
+    if(output.toLowerCase().split(' ').join('').replace(/[^a-zA-Z0-9 ]/g, '') != msg.toLowerCase().split(' ').join('').replace(/[^a-zA-Z0-9 ]/g, '')){
+        return [false, output]
     }
     else {
         return [true, output];
-    }
-    // return results of the check AND reversed string 
-    
+    } 
 }
 
+// displays the result
 function displayResults(results) {
+    // if result return as "TRUE"
     if(results[0] == true){
-        document.getElementById('msg').textContent = `Your message is a palindrome: ${results[1]}`;
+        document.getElementById('msg').textContent = `Your message IS a palindrome: ${results[1]}`;
         document.getElementById('alert').classList.remove('invisible');
     }
+    // If the result returns as "FALSE"
     else{
-        document.getElementById('msg').textContent = `Your message is not a palindrome.`;
+        document.getElementById('msg').textContent = `Your message IS NOT a palindrome: ${results[1]}`;
         document.getElementById('alert').classList.remove('invisible');
     }
 
